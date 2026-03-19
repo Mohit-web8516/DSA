3e8899cc943993a5a0904392d97df1e9f8b73539
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-19 15:31:26 +0000

    array

--- ARRAY/array.cpp
+++ ARRAY/array.cpp
@@ -1,19 +1,43 @@
 //to search an element
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int i, x = 8, index = -1;
-    int arr[5] = {3, 5, 6, 8, 9};
+// int main() {
+//     int i, x = 8, index = -1;
+//     int arr[5] = {3, 5, 6, 8, 9};
 
-    for (i = 0; i < 5; i++) {
-        if (arr[i] == x) {
-            index = i;
-            break;
-        }
-    }
+//     for (i = 0; i < 5; i++) {
+//         if (arr[i] == x) {
+//             index = i;
+//             break;
+//         }
+//     }
 
-    cout << index;
-    return 0;
-}
+//     cout << index;
+//     return 0;
+// }
+
+
+//reverse array
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int arr[6] = {1,2,3,4,5,6};
+
+//   int start = 0,end = 5;
+//   while(start < end){
+//     swap(arr[start],arr[end]);
+//     start++;
+//     end--;
+//   }; 
+//   for (int i =0;i<6;i++)
+//   cout << arr[i]<< " ";
+
+// }
+
+   
+
