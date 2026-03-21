c735573e747c6473eb274c0f926bb75dfea53c2e
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-03-21 17:11:47 +0000

    sorting

--- ARRAY/sorting.cpp
+++ ARRAY/sorting.cpp
@@ -1,25 +1,63 @@
 ///SELECTION SORT
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j;
+//     int arr[6]= {4,7,8,9 ,2,40};
+//     for(int i=0 ;i<5;i++)
+//     {
+//         int index = i;
+//         for(int j=i+1;j<6;j++)
+//         {
+//          if(arr[j]<arr[index])
+//          index = j;
+//         }
+//         swap(arr[i],arr[index]);
+
+//     }
+//     for(int i=0;i<6;i++)
+//     {
+        
+//         cout<<arr[i]<< " ";
+//     }
+// }
+
+//BUBBLE SORT
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int i,j;
-    int arr[6]= {4,7,8,9 ,2,40};
-    for(int i=0 ;i<5;i++)
+    int arr[1000];
+    int n;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    cout<<"enter the element in array: ";
+    for(int i=0 ;i<n;i++)
     {
-        int index = i;
-        for(int j=i+1;j<6;j++)
-        {
-         if(arr[j]<arr[index])
-         index = j;
-        }
-        swap(arr[i],arr[index]);
+        cin>>arr[i];
+    
+    }
+for(int i=n-2;i>=0;i--)
 
-    }
-    for(int i=0;i<6;i++)
+{
+    bool swapped = 0;
+    for(int j=0;j<=i;j++)
     {
-        
-        cout<<arr[i]<< " ";
+        if(arr[j]>arr[j+1]){
+
+            swapped = 1;
+            swap(arr[j],arr[j+1]);
+        }
     }
+       
+    if(swapped==0)
+    break;
+    }
+
+
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
 }
 
